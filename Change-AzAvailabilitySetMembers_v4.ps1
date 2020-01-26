0f627fe4f8b9d11169b6835782ffd45ac4e46c8b
Roelf Zomerman <RZomerman@users.noreply.github.com>
2020-01-26 13:10:48 +0400

    Update Change-AzAvailabilitySetMembers_v4.ps1

--- Change-AzAvailabilitySetMembers_v4.ps1
+++ Change-AzAvailabilitySetMembers_v4.ps1
@@ -394,6 +394,7 @@ If ($Parallel){
                     If ($VMObjectFile.resources.properties.storageProfile.imageReference) {
                         $VMObjectFile.resources.properties.storageProfile.imageReference.id = $null
                         $VMObjectFile.resources.properties.storageProfile.imageReference = $null
+                        $VMObjectFile.resources.properties.storageProfile.psObject.Properties.Remove("ImageReference")
                     }
                     If ($VMObjectFile.resources.properties.osProfile) {
                         $VMObjectFile.resources.properties.osProfile = $null
